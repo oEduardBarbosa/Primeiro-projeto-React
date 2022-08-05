70e48f66d005a5404884db0e5f284245ff03a840
Eduard Barbosa <eb.eduardbarbosa@gmail.com>
2022-08-05 09:14:37 -0300

    Finalizando sidebar

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -19,7 +19,7 @@ export function Sidebar () {
 
             <footer>
                 <a href="https://github.com/oEduardBarbosa">
-                    <PencilLine />
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
